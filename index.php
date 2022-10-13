9cb7223716f2836018dc7b4d79361a0f3213ae2b
Asad2194 <azadhossain0030@gmail.com>
2022-10-13 22:14:56 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="icon" href="images/icon.png">
+    <link rel="stylesheet" href="css/main.css">
+    <title>IJI Restaurant</title>
+</head>
+<body>
+   
+     <div class="navigation-bar" id="home-logo">
+        <div class="navigation-logo ">
+             <a href="index.php"><h1>IJI Restaurant</h1></a>
+        </div>
+        <i class="fa fa-bars fa-2x" aria-hidden="true" id="bar-icon"></i>
+        <span class="hidden" id="Menu-bar-icon">
+            <div class="navigation-link " >
+                <a href="index.php">Home</a>
+                <a href="#speciality">Speciality</a>
+                <a href="#popular">Popular</a>
+                <a href="#gallery">Gallery</a>
+                <a href="#review">Review</a>
+           </div>
+    
+           <div class="navigation-login">
+               <a href="login.php"><h2 class="nav-login">Login</h2></a>
+               <a href="register.php" ><h2 class="nav-register">Sign Up</h2></a>
+           </div>
+        </span>
+    </div>
+
+<section class="home">
+
+    <div class="content">
+        <h3>Food Made With Love</h3>
+        <p>Food is the key element for every human being for the survival on the earth but not every food is that much tasty or applicable for all ages of people.</p>
+        <a href="login.php" class="get-start">Get Started</a>
+    </div>
+
+    <div class="image">
+        <img src="images/home-img.png" alt="Food Image">
+    </div>
+
+</section>
+
+<section class="special" id="speciality">
+    <h1 class="heading"> Our <span>Speciality</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img class="image" src="images/s-img-1.jpg" alt="">
+            <div class="content">
+                <img src="images/s-1.png" alt="">
+                <h3>Tasty Burger</h3>
+                <p>A hamburger, or simply burger, is a food consisting of fillings , usually a patty of ground meat, typically beef, placed inside a sliced bun or bread roll.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/s-img-2.jpg" alt="">
+            <div class="content">
+                <img src="images/s-2.png" alt="">
+                <h3>Tasty Pizza</h3>
+                <p>The pizzas are good too, mainly vegetarian with a mix of tomato or plain base.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/s-img-3.jpg" alt="">
+            <div class="content">
+                <img src="images/s-3.png" alt="">
+                <h3>Cold Ice-Cream</h3>
+                <p>Chocolate-based ice creams are a fan favorite and they are extremely versatile.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/s-img-4.jpg" alt="">
+            <div class="content">
+                <img src="images/s-4.png" alt="">
+                <h3>Cold Drinks</h3>
+                <p>cold drinks a good  description
+soft drink, any of a class of nonalcoholic beverages, usually but not necessarily carbonated, natural or artificial flavours, and sometimes juice.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/s-img-5.jpg" alt="">
+            <div class="content">
+                <img src="images/s-5.png" alt="">
+                <h3>Tasty Sweets</h3>
+                <p>Sweet means syrupy, candied, sugar coated, honeyed, sweetened, sugared,  opposite to bitter, unsweetened or sugarless.</p>
+            </div>
+        </div>
+        <div class="box">
+            <img class="image" src="images/s-img-6.jpg" alt="">
+            <div class="content">
+                <img src="images/s-6.png" alt="">
+                <h3>Healty Breakfast</h3>
+                <p>The best breakfasts for health and weight loss include foods like eggs, berries, avocados, oatmeal, bananas, plain Greek yogurt</p>
+            </div>
+        </div>
+
+    </div>
+</section>
+
+<section class="popular" id="popular">
+
+    <h1 class="heading"> Most <span>Popular</span> Foods </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <span class="price"> 50৳- 200৳ </span>
+            <img src="images/p-1.jpg" alt="">
+            <h3>Tasty Burger</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+        <div class="box">
+            <span class="price"> 200৳- 900৳ </span>
+            <img src="images/p-2.jpg" alt="">
+            <h3>Tasty Cakes</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+        <div class="box">
+            <span class="price"> 250৳- 1500৳ </span>
+            <img src="images/p-3.jpg" alt="">
+            <h3>Tasty Sweets</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+        <div class="box">
+            <span class="price"> 60৳- 900৳ </span>
+            <img src="images/p-4.jpg" alt="">
+            <h3>Tasty Cupcakes</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+        <div class="box">
+            <span class="price"> 80৳- 880৳ </span>
+            <img src="images/p-5.jpg" alt="">
+            <h3>Cold Drinks</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+        <div class="box">
+            <span class="price"> 25৳- 550৳ </span>
+            <img src="images/p-6.jpg" alt="">
+            <h3>Cold Ice-Cream</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <a href="login.php" class="order-btn">Order now</a>
+        </div>
+
+    </div>
+
+</section>
+
+<div class="step-container">
+
+    <h1 class="heading">How it <span>Works</span></h1>
+
+    <section class="steps">
+
+        <div class="box">
+            <img src="images/step-1.jpg" alt="">
+            <h3>Choose Your Favorite Food</h3>
+        </div>
+        <div class="box">
+            <img src="images/step-2.jpg" alt="">
+            <h3>Free And Fast Delivery</h3>
+        </div>
+        <div class="box">
+            <img src="images/step-3.jpg" alt="">
+            <h3>Easy Payments Methods</h3>
+        </div>
+        <div class="box">
+            <img src="images/step-4.jpg" alt="">
+            <h3>And Finally, Enjoy Your Food</h3>
+        </div>
+    
+    </section>
+
+</div>
+
+<section class="gallery" id="gallery">
+
+    <h1 class="heading"> Our Food <span> Gallery </span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/g-1.jpg" alt="">
+            <div class="content">
+                <h3>Burger</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-2.jpg" alt="">
+            <div class="content">
+                <h3>Pizza</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-3.jpg" alt="">
+            <div class="content">
+                <h3>Breakfast</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-4.jpg" alt="">
+            <div class="content">
+                <h3>Cupcakes</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-5.jpg" alt="">
+            <div class="content">
+                <h3>SweetMeat</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-6.jpg" alt="">
+            <div class="content">
+                <h3>Chopes</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-7.jpg" alt="">
+            <div class="content">
+                <h3>Tasty Food</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-8.jpg" alt="">
+            <div class="content">
+                <h3>Sweets</h3>
+            </div>
+        </div>
+        <div class="box">
+            <img src="images/g-9.jpg" alt="">
+            <div class="content">
+                <h3>Dry Food</h3>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="review" id="review">
+
+    <h1 class="heading"> Our Customers <span>Reviews</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="images/pic1.png" alt="Customers picture">
+            <h3>Ayesha Akter</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>It is a great experience. The ambiance is very welcoming and charming. Amazing wines, food and service. Staff are extremely knowledgeable and make great recommendations.</p>
+        </div>
+        <div class="box">
+            <img src="images/pic2.png" alt="">
+            <h3>Niloy Majumdar</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>This cozy restaurant has left the best impressions! Hospitable hosts, delicious dishes, beautiful presentation, wide wine list and wonderful dessert. I recommend to everyone! I would like to come back here again and again.</p>
+        </div>
+        <div class="box">
+            <img src="images/pic3.png" alt="">
+            <h3>Shoshi Sutradhar</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+            </div>
+            <p>This place is great! Atmosphere is chill and cool but the staff is also really friendly. They know what they are doing and what they are talking about, and you can tell making the customers happy is their main priority</p>
+        </div>
+
+    </div>
+
+</section>
+
+<footer class="footer-first">
+    <div class="footer-container">
+        <div class="row">
+            <div class="footer-col">
+                <h4>About Us</h4>
+             <p>One of the most beloved restaurants in Savar,Dhaka.Gramercy Tavern has welcomed guests to enjoy its contemporary cuisine, warm hospitality, and unparalleled service in Savar City for over two decades.</p>
+                <div class="social-links">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </div>
+            </div>
+            <div class="footer-col">
+                <h4>Company</h4>
+                <ul>
+                    <li> <a href="shortlink/faq.php" >FAQ</a></li>
+                    <li> <a href="shortlink/LegalNotice.php" >Legal Notice</a></li>
+                    <li> <a href="shortlink/TermsCondition.php" >Terms And Conditions</a></li>
+                    <li> <a href="shortlink/PrivacyPolicy.php" >Privacy Policy</a></li>
+                    <li> <a href="shortlink/contactUs.php" >About Us</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>online shop</h4>
+                <ul>
+                    <li><i class="fa fa-map-marker" aria-hidden="true" class="address">  <span class="address-in">7 Green Lake Street Crawfordsville, IN 47933</span>  </i></li>
+                    <li><i class="fa fa-phone" aria-hidden="true" class="address">  <span class="address-in">+8801845272299</span></i></li>
+                    <li><i class="fa fa-envelope-o" aria-hidden="true" class="address"> <span class="address-in"> ijilink@help.com</span></i></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h4>Newsletter</h4>
+                 <p>You will be notified when somthing new will be appear.</p>
+                     <form action="" class="footer-form">
+                         <input type="email" name="email" placeholder="Email Address *" required class="footer-input">
+                     </form>
+            </div>
+        </div>
+    </div>
+</footer>
+<hr>
+
+<div class="footer-second">
+    <div class="footer-sec-in">
+
+        <img src="images/payment.png" alt="payment method">
+
+        <p class="copyright">
+          Copyright &copy; 2022 <span>Md Asaduzzaman</span> all rights reserved.
+        </p>
+
+      </div>
+</div>
+
+ <script src="javascript/main.js"></script>
+
+</body>
+</html>
